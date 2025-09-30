57c5912661be389f7493a0bfe68d100e920ad569
Alvaro Fonteles <alvarofonteles@hotmail.com>
2025-09-30 08:50:51 -0300

    Extras

---
+++ extras/01_extra.py
@@ -0,0 +1,15 @@
+print('Deus é bom!')
+
+# Formatar números
+preco = 99.9
+print(f'Preço: R$ {preco:.2f}')  # Preço: R$ 99.90
+
+# Expressões diretas
+numero = 4
+print(f'Dobro: {numero * 2}')
+
+# Chamar funções
+nome = 'Alvaro'
+profissao = 'Engenheiro de Dados'
+print(f'Que bacana {nome.upper()}, {profissao.upper()} '
+      f'é uma excelente profissão, pagando {preco:.2f} na hora.')
