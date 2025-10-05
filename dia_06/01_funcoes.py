cb3a9c2b8ac5fc95a42914e6196acc0f59b641ec
Alvaro Fonteles <alvarofonteles@hotmail.com>
2025-10-05 09:39:32 -0300

    Dia 06 e Extras P1

---
+++ dia_06/01_funcoes.py
@@ -0,0 +1,34 @@
+# Funções são blocos de código reutilizáveis que executam uma tarefa específica.
+# Elas ajudam a organizar o código, tornando-o mais legível e modular.
+
+# Definindo uma função simples que imprime uma mensagem de boas-vindas
+
+# %%
+def boas_vindas():
+    print("Bem-vindo ao curso de Python!")
+
+# Chamando a função
+boas_vindas()
+# Função com parâmetros
+def saudacao(nome):
+    print(f"Olá, {nome}! Como você está?")
+
+# Chamando a função com um argumento
+saudacao("Maria")
+
+# Função Soma
+# Na Função abaixo passa-se assinatura, existe um bloco que executa sem retorno
+# e um retorno como positivo
+# %%
+def calc_soma(numero):
+    if numero >= 10: return f'número {numero} inválido'
+
+    for x in range(1, 11): print(f'{x} + {numero} = {x + numero}')
+
+    return f'Parabéns, sucesso!'
+ 
+# %%
+valor = int(input(f'Digite um numero de 1 - 10: '))
+resultado = calc_soma(valor)
+print(resultado)
+
---
+++ extras/06_extras.py
@@ -0,0 +1,54 @@
+# Tabuada de Soma
+# %%
+def tabuada_soma(numero, operador):    
+    operadores = ['+', '-', '/', '*'] # para algo escalável, futuro
+
+    if operador not in operadores:
+        return f'Operador ({operador}) Inválido'
+
+    if operador == '+':    
+        if numero == 0: # executa toda a tabela
+            for soma in range(1, 11):
+                print('')
+                for num_soma in range(1, 11):
+                    print(f'{soma} + {num_soma} = {soma + num_soma}')
+        else:
+            for num_soma in range(1, 11):
+                print(f'{numero} + {num_soma} = {numero + num_soma}')
+
+# Calculadora
+# %%
+def calculadora(num1, num2, operador):
+    operadores = ['+', '-', '/', '*']
+    
+    if operador not in operadores:
+        return f'Operador ({operador}) Inválido'
+    
+    if operador == '+':
+        return num1 + num2
+    elif operador == '-':
+        return num1 - num2
+    elif operador == '*':
+        return num1 * num2
+    elif operador == '/':
+        if num2 == 0:
+            return 'Erro: Divisão por zero'
+        return num1 / num2
+
+# Tabuada de Soma
+# %%
+numero = int(input(f'Digite um numero de 1 - 10 \ne 0 para Toda Tabuada de Soma: '))
+operador = input(f'Digite Operador (+) Numérico: ')
+
+tabuada_soma(numero, operador)
+
+# %%
+# Calculadora
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+operador = input('Digite o operador (+, -, *, /): ')
+
+resultado = calculadora(num1, num2, operador)
+print(f'Resultado: {resultado}')
+
+
