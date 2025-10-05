07169a6e43bf446ecc6d7678bf76bf79cda33ede
Alvaro Fonteles <alvarofonteles@hotmail.com>
2025-10-05 18:02:10 -0300

    Extras P1

--- extras/07_extra.py
+++ extras/07_extra.py
@@ -22,3 +22,53 @@ print(f'Média 2: {media_2(*vlr_2)}')
 
 
 # %%
+def soma(a:float, b:float, *args) ->float:
+    vlr = [a, b] + list(args) 
+    return sum(vlr) 
+
+vls = soma(2, 5, 7)
+print(vls)
+
+# %%
+def soma(a:float, b:float, **kwargs)->float:
+    vlr = [a, b] + list(kwargs.values())  # pega só os valores
+    return sum(vlr)
+
+# Agora funciona:
+vls = soma(2, 5, c=7, d=10)  # 24
+print(vls)
+
+# %%
+# *args → TUPLA de valores
+def com_args(*args):
+    print(args)  # (2, 5, 7, 10) - SÓ VALORES
+
+com_args(2, 5, 7, 10)
+
+# %%
+# **kwargs → DICIONÁRIO chave:valor  
+def com_kwargs(**kwargs):
+    print(kwargs)  # {'c': 7, 'd': 10} - CHAVES E VALORES
+    print(kwargs.keys())
+    print(kwargs.values())
+    print(kwargs.items())
+
+com_kwargs(c=3, d=10)   
+
+# %% Transforma em Lista
+def com_kwargs_2(**kwargs):
+    valores = list(kwargs.values())  # [7, 10] - LISTA
+    chaves = list(kwargs.keys())     # ['c', 'd'] - LISTA  
+    itens = list(kwargs.items())     # [('c', 7), ('d', 10)] - LISTA
+    
+    for chave, valor in kwargs.items():
+        print(f"{chave} = {valor}")
+    # c = 7
+    # d = 10
+    print(valores)
+    print(chaves)
+    print(itens)
+
+com_kwargs_2(c=3, d=10)
+
+# %%
